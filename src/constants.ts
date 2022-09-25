06ab07ecf08712ab8426147f6fdf0aaeba4de7ec
soyboy <sbvegan@protonmail.com>
2022-09-25 02:40:28 -0700

    adding art gobblers constants

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+// ADDRESSES
+export const ART_GOBBLERS_ADDRESS: string = '';
+export const GOO_ADDRESS: string = '';
+export const PAGES_ADDRESS: string = '';
+export const TEAM_ADDRESS: string = '';
+export const COMMUNITY_ADDRESS: string = '';
+// SUPPLY
+export const MAX_SUPPLY: number = 10000;
+export const MINTLIST_SUPPLY: number = 2000;
+export const LEGENDARY_SUPPLY: number = 10;
+export const RESERVED_SUPPLY: number = (MAX_SUPPLY - MINTLIST_SUPPLY - LEGENDARY_SUPPLY) / 5;
+export const MAX_MINTABLE = MAX_SUPPLY - MINTLIST_SUPPLY - LEGENDARY_SUPPLY - RESERVED_SUPPLY;
+// METADATA CONSTANTS
+export const UNREVEALED_URI: string = '';
+export const BASE_URI: string = '';
+
+

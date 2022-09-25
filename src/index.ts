055ae7c482d54fbcb23318b65789d90be387be29
soyboy <sbvegan@protonmail.com>
2022-09-25 02:40:38 -0700

    adding alchemy sdk

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-import dotenv from "dotenv"
-dotenv.config()
+// const ethers = require("ethers");
+const artGobblersABI = require("./abis/art-gobblers.json"); 
+import { ART_GOBBLERS_ADDRESS } from "./constants";
+require("dotenv").config()
+const { Alchemy, Network } = require("@alch/alchemy-web3");
 
-console.log("hello gobblers")
-console.log(process.env.SECRET)
+async function main() {
+    const settings = {
+        apiKey: "demo", // Replace with your Alchemy API Key.
+        network: Network.ETH_MAINNET, // Replace with your network.
+    };
+      
+    const alchemy = new Alchemy(settings);
+    // https://docs.alchemy.com/reference/subscription-api#alchemy-sdks-event-types
+    // todo: add filter
+    // todo: add subscriptions:
+    // alchemy.ws.on("", () => {
+
+    // })
+}
+
+main();
